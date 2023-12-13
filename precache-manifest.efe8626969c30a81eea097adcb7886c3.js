9fd30fb152f0236c2c3e24c819f1e37637e071b7
binayakdhakal <binayakdhakal999@gmail.com>
2023-12-14 00:30:49 +0545

    new update

---
+++ precache-manifest.efe8626969c30a81eea097adcb7886c3.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495283fc07095584be92",
+    "url": "/css/Login.f8dda1fd.css"
+  },
+  {
+    "revision": "309444c657ef91dce719",
+    "url": "/css/Register.363e8db9.css"
+  },
+  {
+    "revision": "c5c06f02c081679c84db",
+    "url": "/css/Verify.09ec16ee.css"
+  },
+  {
+    "revision": "6d404ada03fa691a7edb",
+    "url": "/css/app.c6bd0cd2.css"
+  },
+  {
+    "revision": "55e4d2167a356bc7bec6",
+    "url": "/css/chunk-02b00f48.07ec8502.css"
+  },
+  {
+    "revision": "592c5c1d9af62e46c440",
+    "url": "/css/chunk-0a5dc364.098a8088.css"
+  },
+  {
+    "revision": "d4ced7b078d4527c09be",
+    "url": "/css/chunk-19770aa8.40650297.css"
+  },
+  {
+    "revision": "9b62cd5288f7019d55d2",
+    "url": "/css/chunk-1df15a48.02d0e8da.css"
+  },
+  {
+    "revision": "c09dbceab4eb75eea5b2",
+    "url": "/css/chunk-22126d0a.63906551.css"
+  },
+  {
+    "revision": "ebc38ab127cf5288aeca",
+    "url": "/css/chunk-2226c7e2.c686900f.css"
+  },
+  {
+    "revision": "655702a3ccfc96b887a8",
+    "url": "/css/chunk-240564c2.ccf1f5ce.css"
+  },
+  {
+    "revision": "6bae59705d556da6b363",
+    "url": "/css/chunk-2f28d06a.9ff729a9.css"
+  },
+  {
+    "revision": "25199a5d3e3b2ed99816",
+    "url": "/css/chunk-382109c7.bb6db235.css"
+  },
+  {
+    "revision": "185f4260470d3839549d",
+    "url": "/css/chunk-3a6ebf0b.9d21c0d7.css"
+  },
+  {
+    "revision": "fc3049d4325d32bc7daa",
+    "url": "/css/chunk-3f94b867.23996a41.css"
+  },
+  {
+    "revision": "c986bf9b6ed71eeffe0d",
+    "url": "/css/chunk-4150934f.e8952b4b.css"
+  },
+  {
+    "revision": "b6d18adc317cef8fdb55",
+    "url": "/css/chunk-4b683b3d.3116f913.css"
+  },
+  {
+    "revision": "804fba3441823f4e0d6d",
+    "url": "/css/chunk-4ca5c55c.940edce1.css"
+  },
+  {
+    "revision": "2d9a9584c348bd154928",
+    "url": "/css/chunk-4d09afe8.c797edc5.css"
+  },
+  {
+    "revision": "baeacbaeebb7b244446f",
+    "url": "/css/chunk-76f08a5d.4412413a.css"
+  },
+  {
+    "revision": "284ad81812f6dc7bb2df",
+    "url": "/css/chunk-875e4d06.8689fec2.css"
+  },
+  {
+    "revision": "d539ee4c35142fd50a13",
+    "url": "/css/chunk-958841dc.fbd83163.css"
+  },
+  {
+    "revision": "0a16f66ad83a96fd79d2",
+    "url": "/css/chunk-c0ca0b26.777251f5.css"
+  },
+  {
+    "revision": "d81638e36528390d00fb",
+    "url": "/css/chunk-efb8ba7e.9ce2b933.css"
+  },
+  {
+    "revision": "2648934a5986653b1edc",
+    "url": "/css/chunk-vendors.7e7845f5.css"
+  },
+  {
+    "revision": "7e54065a5c895822ea0f1a25d38de54e",
+    "url": "/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "7e3aef1e438425303d4de774be918692",
+    "url": "/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "0fa327cc98bb49ab027b2c10ee0db257",
+    "url": "/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "f273d9fe7fe20bf68300abcc41bd020e",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "b8999884d1d24f2350cb7d9af14e3612",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "e90426b2f0ed5a893cbe21633cde1bd4",
+    "url": "/img/logo.e90426b2.png"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/img/site.webmanifest"
+  },
+  {
+    "revision": "4bf9a74eff73b5e76af0019ce8fb9c0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "495283fc07095584be92",
+    "url": "/js/Login.8be793e8.js"
+  },
+  {
+    "revision": "309444c657ef91dce719",
+    "url": "/js/Register.dccb4711.js"
+  },
+  {
+    "revision": "c5c06f02c081679c84db",
+    "url": "/js/Verify.3dabb71d.js"
+  },
+  {
+    "revision": "6d404ada03fa691a7edb",
+    "url": "/js/app.c2b7b1c5.js"
+  },
+  {
+    "revision": "55e4d2167a356bc7bec6",
+    "url": "/js/chunk-02b00f48.27cddd5d.js"
+  },
+  {
+    "revision": "592c5c1d9af62e46c440",
+    "url": "/js/chunk-0a5dc364.e0c9d5f0.js"
+  },
+  {
+    "revision": "d4ced7b078d4527c09be",
+    "url": "/js/chunk-19770aa8.5096c07b.js"
+  },
+  {
+    "revision": "9b62cd5288f7019d55d2",
+    "url": "/js/chunk-1df15a48.29627eaf.js"
+  },
+  {
+    "revision": "c09dbceab4eb75eea5b2",
+    "url": "/js/chunk-22126d0a.938a750a.js"
+  },
+  {
+    "revision": "ebc38ab127cf5288aeca",
+    "url": "/js/chunk-2226c7e2.b545ac92.js"
+  },
+  {
+    "revision": "655702a3ccfc96b887a8",
+    "url": "/js/chunk-240564c2.e29404b1.js"
+  },
+  {
+    "revision": "6bae59705d556da6b363",
+    "url": "/js/chunk-2f28d06a.34bd44d1.js"
+  },
+  {
+    "revision": "25199a5d3e3b2ed99816",
+    "url": "/js/chunk-382109c7.0dd85d2d.js"
+  },
+  {
+    "revision": "185f4260470d3839549d",
+    "url": "/js/chunk-3a6ebf0b.2d085ca5.js"
+  },
+  {
+    "revision": "fc3049d4325d32bc7daa",
+    "url": "/js/chunk-3f94b867.b7c900e3.js"
+  },
+  {
+    "revision": "c986bf9b6ed71eeffe0d",
+    "url": "/js/chunk-4150934f.ba0abb32.js"
+  },
+  {
+    "revision": "b6d18adc317cef8fdb55",
+    "url": "/js/chunk-4b683b3d.651ac433.js"
+  },
+  {
+    "revision": "804fba3441823f4e0d6d",
+    "url": "/js/chunk-4ca5c55c.0b5ca2e8.js"
+  },
+  {
+    "revision": "2d9a9584c348bd154928",
+    "url": "/js/chunk-4d09afe8.15ba9a18.js"
+  },
+  {
+    "revision": "baeacbaeebb7b244446f",
+    "url": "/js/chunk-76f08a5d.09f0e27c.js"
+  },
+  {
+    "revision": "284ad81812f6dc7bb2df",
+    "url": "/js/chunk-875e4d06.55e1293c.js"
+  },
+  {
+    "revision": "d539ee4c35142fd50a13",
+    "url": "/js/chunk-958841dc.640cb592.js"
+  },
+  {
+    "revision": "0a16f66ad83a96fd79d2",
+    "url": "/js/chunk-c0ca0b26.2f6de580.js"
+  },
+  {
+    "revision": "d81638e36528390d00fb",
+    "url": "/js/chunk-efb8ba7e.1e746f32.js"
+  },
+  {
+    "revision": "2648934a5986653b1edc",
+    "url": "/js/chunk-vendors.b6d0732c.js"
+  },
+  {
+    "revision": "1246197eaf708fc68a1263ee4d19bbde",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4aeebab096ac2109dc5c27be7784f40e",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bb5ae3323ac238bc516f6d383d1b13b0",
+    "url": "/sitemap.xml"
+  }
+]);
